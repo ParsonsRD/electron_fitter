520694079c51447fb887e9c19bfed317286f6067
Dan Parsons <parsonsrd@gmail.com>
2019-06-06 10:39:36 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ElectronFitter',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/ParsonsRD/electron_fitter',
+    license='',
+    author='dparsons',
+    author_email='',
+    description=''
+)
